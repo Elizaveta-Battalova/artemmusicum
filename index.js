a007377cf346f97af4384e1a5b43b93ae93841e5
Liza <elizaveta.battalova@rambler.ru>
2022-01-17 00:18:51 +0300

    update toggle button bootstrap

---
+++ index.js
@@ -0,0 +1,5 @@
+// function hideMenu() {
+//     let item = document.getElementById("dropdown-content");
+//     if (item.style.display == "block") item.style.display = "none";
+//     else item.style.display = "block";
+// }
