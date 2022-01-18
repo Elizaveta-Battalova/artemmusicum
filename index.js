b8bea0cebe66f6643498e595c6abc0589abbf50f
Liza <elizaveta.battalova@rambler.ru>
2022-01-18 16:09:03 +0300

    try smooth hook

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ window.addEventListener("click", function(event) {
         dropdownListContainer.classList.remove("show");
     }
 });
+
+const anchors = document.querySelectorAll('a.scroll-to')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault()
+
+        const blockID = anchor.getAttribute('href')
+
+        document.querySelector(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
